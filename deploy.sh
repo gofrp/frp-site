7efa53f92b74bb365b87f254087cd94fa13e02aa
fatedier <fatedier@gmail.com>
2017-02-27 01:22:25 +0800

    auto deploy

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,16 @@
 
 # git clone frp-doc
 SITE_ROOT=`pwd`
-cd ../
 if [ -d "./frp-doc" ]; then
     cd ./frp-doc
     git pull
 else
-    git pull https://github.com/fatedier/frp-doc.git
-    cd ./frp-doc
+    git clone https://github.com/fatedier/frp-doc.git frp-doc
 fi
 
 cd $SITE_ROOT
 # copy docs to site
-cp -rf ../frp-doc/zh-cn/* ./source/
+cp -rf ./frp-doc/zh-cn/* ./source/
 
 hexo clean
 hexo generate
