4c4858ab4c13212cd21366300a0cd1ab7e8cf20d
fatedier <fatedier@gmail.com>
2017-02-27 00:51:56 +0800

    move docs to frp-doc

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
+
+# git clone frp-doc
+SITE_ROOT=`pwd`
+cd ../
+if [ -d "./frp-doc" ]; then
+    cd ./frp-doc
+    git pull
+else
+    git pull https://github.com/fatedier/frp-doc.git
+    cd ./frp-doc
+fi
+
+cd $SITE_ROOT
+# copy docs to site
+cp -rf ../frp-doc/zh-cn/* ./source/
+
 hexo clean
 hexo generate
