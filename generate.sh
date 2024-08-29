f392fc8dd367dd96899e160def9994ecfb1792b8
fatedier <fatedier@gmail.com>
2024-08-29 15:34:42 +0800

    update hugo version

--- generate.sh
+++ generate.sh
@@ -4,8 +4,8 @@ SITE_ROOT=`pwd`
 cd ./source
 npm install
 
-wget https://github.com/gohugoio/hugo/releases/download/v0.119.0/hugo_extended_0.119.0_Linux-64bit.tar.gz
-tar -zxvf ./hugo_extended_0.119.0_Linux-64bit.tar.gz
+wget https://github.com/gohugoio/hugo/releases/download/v0.133.1/hugo_extended_0.133.1_Linux-64bit.tar.gz
+tar -zxvf ./hugo_extended_0.133.1_Linux-64bit.tar.gz
 ./hugo
 
 if [ -d "./public" ]; then
